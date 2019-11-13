ba160cb4eb370e1d5d9c8ae778d612679f94e805
marcusmckinley <marcus.mckinley13@gmail.com>
2019-11-13 18:32:44 -0500

    trying to fix display look

--- java/grade1.java
+++ java/grade1.java
@@ -165,6 +165,7 @@ class grade1 {
     for (int i = 1; i <= columnsNumber; i++) {
       String column_name = rsmd.getColumnName(i);
       System.out.print(column_name);
+      System.out.print(column_name.length());
       for (int j = 1; j <= 20 - column_name.length(); i++) {
         System.out.print(" ");
       }
